7370d6117a3442fe903f367b90854fbe465b2a14
Orina <oimbunche@gmail.com>
2025-01-18 15:44:45 +0300

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div className='flex justify-center bg-slate-100 p-3'>
+      <h1>ORINA</h1>
+    </div>
+  )
+}
+
+export default page
